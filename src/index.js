3edcef8efafa6dd64e7ee2668b719f7044d441aa
nicolas703 <60550350+nicolas703@users.noreply.github.com>
2020-05-25 21:32:48 -0400

    jumbotron y navbar

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as NavBar from "./navBar";
+import * as Yumbotron from "./jumbo"
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App(){
+
+    return(
+        <>    
+            <NavBar.Bar/>
+            <div> 
+            <Yumbotron.Yumbo/>
+            </div>
+
+        </>
+        )
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"));
---
+++ src/navBar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export function Bar(){
+    return (
+        <>
+            
+                <div className="navbar navbar-expand-lg navbar-dark bg-dark container-fluid active">
+                    <nav className="navbar navbar-expand-lg container active">
+                        <a className="navbar-brand" href="#">
+                            Start Bootstrap
+                        </a>
+                        <ul className="navbar-nav ml-auto">
+
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Home </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Services</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+         
+        </>
+    )
+}
